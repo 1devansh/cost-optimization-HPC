b86d0e44410eba38022fb82170e1df76a8cbaea0
Devansh Sharma <ds8611@bennett.edu.in>
2020-05-23 22:54:48 +0530

    graph

--- main.py
+++ main.py
@@ -2,13 +2,7 @@ from __future__ import division
 import random
 import math
 from itertools import combinations
-
 import matplotlib.pyplot as plt
-
-
-p = 128
-
-
 
 def fact(n):
     return math.factorial(n)
@@ -21,31 +15,30 @@ alpha = random.randint(85,96)
 
 #comb = combinations(p, v)
 #R = alpha *v + (1-alpha) * (p-v) * 70
-
-
+p = 128
 CpiR = 0.2
 CpiV = 0.02
 Cw = 1
-
 beta = 0.3
-
 alpha = 0.85
 
 Tpar = (beta * 12) + ((1-beta) * 12)/p
 
-
 C = 0
-
 P =[]
 c = []
-for i in range(p):
-    P.append(p)
-    R = (alpha ** i) * ((1-alpha)**(p-i)) * comb(p,i)
-    C += ((Tpar * R) * ((p - i) * CpiR + i * CpiV))
-C =C + Tpar * Cw
 
-plt.plot(C,)
+for i in range(1,p+1):
+    P.append(i)
+    for j in range(1,i+1):
+        
+        R = (alpha ** j) * ((1-alpha)**(i-j)) * comb(i,j)
+        C += ((Tpar * R) * ((i - j) * CpiR + j * CpiV))
+    C =C + Tpar * Cw
+    c.append(C)
+
+plt.plot(P,c)
 plt.show()
 plt.title('Cost for parallel computations with different scalabilty')
-
-print(C)
+print(P)
+print(c)
