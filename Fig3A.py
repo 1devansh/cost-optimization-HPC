3420cdae7ff2017c3b7b5ecdfc0f31728da97b0f
Devansh Sharma <ds8611@bennett.edu.in>
2020-05-24 20:19:32 +0530

    Fig 3 Part1 success

--- Fig3A.py
+++ Fig3A.py
@@ -1,21 +1,29 @@
 from __future__ import division
 import random
 import math
+from mpl_toolkits import mplot3d
 from itertools import combinations
 import matplotlib.pyplot as plt
+
 
 def fact(n):
     return math.factorial(n)
 
+
 def comb(p, v):
     return fact(p)/(fact(v)*fact(p-v))
 
-def graph(p, beta, alpha, CpiR, CpiV, Cw, temp):
+
+def graph(p, beta, alpha, CpiR, CpiV, Cw):
     P = []
     c = []
+    alph =[]
+    f = alpha
     for i in range(1, p+1):
         C = 0
         P.append(i)
+        alph.append(alpha)
+        
         Tpar = (beta[temp] * 12) + ((1-beta[temp]) * 12)/i
 
         for j in range(i):
@@ -23,7 +31,7 @@ def graph(p, beta, alpha, CpiR, CpiV, Cw, temp):
             C += ((Tpar * R) * ((i - j) * CpiR + j * CpiV))
         C = C + Tpar * Cw
         c.append(C)
-    return P,c
+    return P, c, alph
 
 
 temp = 4
@@ -33,17 +41,20 @@ CpiR = 0.2
 CpiV = 0.02
 Cw = 1
 beta = [0.01, 0.05, 0.1, 0.2, 0.3]
-alpha = 0.85
-fig, (one) = plt.subplots(1, 1)
+alpha = 1
+fig = plt.figure()
+one = plt.axes(projection="3d")
+f = alpha
+while alpha >0:
+    alpha = alpha - f/128
+    L, M, A = graph(p, beta, alpha, CpiR, CpiV, Cw)
+    one.plot3D(L, A, M)
 
-for temp in range(5):
-    L,M = graph(p, beta, alpha, CpiR, CpiV, Cw, temp)
-    one.plot(L,M, label = 'beta = '+str(beta[temp]))
 
-
-fig.suptitle('Cost for parallel computations with different scalabilty till p = 128')
-one.set_ylabel('Cost(p)')
+fig.suptitle(
+    'Cost for parallel computations with volatile process of different availability')
+one.set_ylabel('Alpha')
 one.set_xlabel('No. of processors (P)')
+one.set_zlabel('Cost(p)')
 one.legend()
 plt.show()
-
